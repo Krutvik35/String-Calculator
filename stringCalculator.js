ae110552efac130c92fc282edadd7300b8ac63ba
Krutvik35 <194krutvik@gmail.com>
2024-06-13 10:47:15 +0530

    Add method to handle new lines between numbers as seperator

--- stringCalculator.js
+++ stringCalculator.js
@@ -5,7 +5,9 @@ function add(numbers) {
 		return 0;
 	}
 
-	const numArray = numbers.split(",");
+	const delimiters = [",", "\n"]; // Array of delimiters
+	const regex = new RegExp(delimiters.join("|"), "g");
+	const numArray = numbers.split(regex);
 	return numArray.reduce((sum, num) => sum + parseInt(num), 0);
 }
 
@@ -14,5 +16,6 @@ console.log("Input => null Output =>", add(null));
 console.log('Input => "1" Output =>', add("1"));
 console.log('Input => "1,5" Output =>', add("1,5"));
 console.log('Input => "1,3,5,7" Output =>', add("1,3,5,7"));
+console.log('Input => "1\\n2,3,4" Output =>', add("1\n2,3,4"));
 
 module.exports = add;
--- stringCalculator.test.js
+++ stringCalculator.test.js
@@ -24,3 +24,7 @@ test("should return NaN if string has invalid numbers", () => {
 test("should return the sum of multiple numbers", () => {
 	expect(add("1,2,3,4")).toBe(10);
 });
+
+test("should return the sum of numbers with new line", () => {
+	expect(add("1\n2,3")).toBe(6);
+});
