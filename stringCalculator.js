54557204bb6fa0d57466e8af1877a6ab8d6d2989
Krutvik35 <194krutvik@gmail.com>
2024-06-13 10:30:06 +0530

    Add a simple string calculator with one or two number support

--- stringCalculator.js
+++ stringCalculator.js
@@ -5,10 +5,17 @@ function add(numbers) {
 		return 0;
 	}
 
-	return parseInt(numbers);
+	const numArray = numbers.split(",");
+	if (numArray.length === 1) {
+		return parseInt(numArray[0]);
+	}
+
+	return parseInt(numArray[0]) + parseInt(numArray[1]);
 }
 
 console.log('Input => "" Output =>', add(""));
 console.log("Input => null Output =>", add(null));
+console.log('Input => "1" Output =>', add("1"));
+console.log('Input => "1,5" Output =>', add("1,5"));
 
 module.exports = add;
--- stringCalculator.test.js
+++ stringCalculator.test.js
@@ -1,10 +1,22 @@
 // Import add function from stringCalculator
 const add = require("./stringCalculator");
 
-test("returns 0 for an empty string", () => {
+test("should return 0 for an empty string", () => {
 	expect(add("")).toBe(0);
 });
 
-test("returns 0 for null value", () => {
+test("should return 0 for null value", () => {
 	expect(add(null)).toBe(0);
+});
+
+test("should return the number itself for a single number", () => {
+	expect(add("1")).toBe(1);
+});
+
+test("should return the sum of two numbers", () => {
+	expect(add("10,5")).toBe(15);
+});
+
+test("should return NaN if string has invalid numbers", () => {
+	expect(add("test")).toBe(NaN);
 });
